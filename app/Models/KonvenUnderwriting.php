7da672f39b030d56a47e6f7fe10965c72ada5896
Ramdoni <doni.engine@gmail.com>
2021-02-08 13:29:46 +0700

    konven uw guarded

--- app/Models/KonvenUnderwriting.php
+++ app/Models/KonvenUnderwriting.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class KonvenUnderwriting extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 
     protected $table = 'konven_underwriting';
 
