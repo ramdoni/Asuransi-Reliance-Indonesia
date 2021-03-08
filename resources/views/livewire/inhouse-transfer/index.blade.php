bafdcacd87c16c31c16dd7dde30f5b05a4d32a3d
Ramdoni <doni.engine@gmail.com>
2021-03-09 06:03:22 +0700

    inhouse transfer edit label

--- resources/views/livewire/inhouse-transfer/index.blade.php
+++ resources/views/livewire/inhouse-transfer/index.blade.php
@@ -6,7 +6,7 @@
             <div class="body">
                 <ul class="nav nav-tabs">
                     <li class="nav-item"><a class="nav-link active show" data-toggle="tab" href="#inhouse-transfer">{{ __('Inhouse Transfer') }}</a></li>
-                    <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#bank-account">{{ __('Bank Account') }}</a></li>
+                    <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#bank-account">{{ __('Balance') }}</a></li>
                 </ul>
                 <div class="px-0 tab-content">
                     <div class="tab-pane show active" id="inhouse-transfer">
