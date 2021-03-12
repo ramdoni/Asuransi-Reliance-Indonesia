865d88934eec8549eb8bda31d6d52aad11981c53
Ramdoni <doni.engine@gmail.com>
2021-03-12 11:33:32 +0700

    Update claim payable-v

--- resources/views/livewire/expense-claim/insert.blade.php
+++ resources/views/livewire/expense-claim/insert.blade.php
@@ -52,7 +52,7 @@
                     <div class="form-group">
                         <label>{{ __('From Bank Account') }}</label>
                         <select class="form-control" wire:model="from_bank_account_id">
-                            <option value=""> --- None --- </option>
+                            <option value=""> --- Select --- </option>
                             @foreach (\App\Models\BankAccount::where('is_client',0)->orderBy('owner','ASC')->get() as $bank)
                                 <option value="{{ $bank->id}}">{{ $bank->owner }} - {{ $bank->no_rekening}} {{ $bank->bank}}</option>
                             @endforeach
@@ -64,7 +64,7 @@
                     <div class="form-group" wire:ignore>
                         <label>{{ __('To Bank Account') }}</label>
                         <select class="form-control select_to_bank" id="to_bank_account_id" wire:model="to_bank_account_id">
-                            <option value=""> --- Select --- </option>
+                            <option value=""> --- None --- </option>
                             @foreach (\App\Models\BankAccount::where('is_client',1)->orderBy('owner','ASC')->get() as $bank)
                                 <option value="{{ $bank->id}}">{{ $bank->owner }} - {{ $bank->no_rekening}} {{ $bank->bank}}</option>
                             @endforeach
